690b8cf7497a5386ed7fda7e28db9e6f1b20dee7
Yusuke Tuble Adachi <adachiab@gmail.com>
2024-03-15 16:08:26 +0900

    gh-pages commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to My Landing Page</h1>
+                <p>This is a simple React-based landing page for GitHub Pages.</p>
+            </header>
+        </div>
+        );
+}
+
+export default App;
